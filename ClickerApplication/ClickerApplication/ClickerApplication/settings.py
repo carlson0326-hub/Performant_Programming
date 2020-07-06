22f5a53e178defa29fa3874900c02ec7b202dc23
Alexalonso04 <alexalonso04@ufl.edu>
2020-07-05 23:09:20 -0400

    finished the set up for basic question and answer slection

--- ClickerApplication/ClickerApplication/ClickerApplication/settings.py
+++ ClickerApplication/ClickerApplication/ClickerApplication/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'Quiz'
+    'Quiz.apps.QuizConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ClickerApplication.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
